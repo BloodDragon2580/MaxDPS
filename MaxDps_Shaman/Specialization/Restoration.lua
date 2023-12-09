9f418f2913b8853c80ea3ef1b4523e2346b7b904
BloodDragon2580 <gewinner.thomas@gmail.com>
2023-12-09 13:01:34 +0100

    Update schami

--- MaxDps_Shaman/Specialization/Restoration.lua
+++ MaxDps_Shaman/Specialization/Restoration.lua
@@ -1,99 +1,57 @@
-
-local _, addonTable = ...
+local _, addonTable = ...;
 
 --- @type MaxDps
 if not MaxDps then return end
 
-local Shaman = addonTable.Shaman
-local MaxDps = MaxDps
-local UnitPower = UnitPower
-local UnitHealth = UnitHealth
-local UnitAura = UnitAura
-local GetSpellDescription = GetSpellDescription
-local UnitHealthMax = UnitHealthMax
-local UnitPowerMax = UnitPowerMax
-local PowerTypeMaelstrom = Enum.PowerType.Maelstrom
+local MaxDps = MaxDps;
+local Shaman = addonTable.Shaman;
 
-local fd
-local cooldown
-local buff
-local debuff
-local talents
-local targets
-local targetHP
-local targetmaxHP
-local targethealthPerc
-local curentHP
-local maxHP
-local healthPerc
-
-local className, classFilename, classId = UnitClass('player')
-local currentSpec = GetSpecialization()
-local currentSpecName = currentSpec and select(2, GetSpecializationInfo(currentSpec)) or "None"
-local classtable
+local RT = {
+	ChainLightning = 188443,
+	EarthlivingWeapon = 382021,
+	EarthlivingEnchantId = 6498,
+	FlameShock = 188389,
+	LightningBolt = 188196,
+	LavaBurst = 51505,
+	WaterShield = 52127
+};
 
 function Shaman:Restoration()
-    fd = MaxDps.FrameData
-    cooldown = fd.cooldown
-    buff = fd.buff
-    debuff = fd.debuff
-    talents = fd.talents
-    targets = MaxDps:SmartAoe()
-    targetHP = UnitHealth('target')
-    targetmaxHP = UnitHealthMax('target')
-    targethealthPerc = (targetHP / targetmaxHP) * 100
-    curentHP = UnitHealth('player')
-    maxHP = UnitHealthMax('player')
-    healthPerc = (curentHP / maxHP) * 100
-    classtable = MaxDps.SpellTable
-	classtable.LavaSurgeBuff = 77762
-	setmetatable(classtable, Shaman.spellMeta)
-    if targets >= 3  then
-        return Shaman:RestorationMultiTarget()
-    end
-    return Shaman:RestorationSingleTarget()
-end
+	local fd = MaxDps.FrameData;
+	local cooldown = fd.cooldown;
+	local debuff = fd.debuff;
+	local buff = fd.buff;
 
---optional abilities list
+	MaxDps:GlowCooldown(RT.WaterShield, not buff[RT.WaterShield].up);
 
+	local hasMainHandEnchant, mainHandExpiration, _, mainHandEnchantID, _, _, _, _ = GetWeaponEnchantInfo();
+	local earthLivingEnchantRemaining = false;
 
---Single-Target Rotation
-function Shaman:RestorationSingleTarget()
-	if not debuff[classtable.FlameShock].up or debuff[classtable.FlameShock].refreshable and cooldown[classtable.FlameShock].ready then
-		return classtable.FlameShock
+	if hasMainHandEnchant then
+		if mainHandEnchantID == RT.EarthlivingEnchantId then
+			earthLivingEnchantRemaining = mainHandExpiration / 1000;
+		end
 	end
-	if talents[classtable.Stormkeeper] and cooldown[classtable.Stormkeeper].ready then
-		return classtable.Stormkeeper
-	end
-	if cooldown[classtable.LavaBurst].ready then
-		return classtable.LavaBurst
-	end
-	if cooldown[classtable.LightningBolt].ready then
-		return classtable.LightningBolt
-	end
-end
 
---Multiple-Target Rotation
-function Shaman:RestorationMultiTarget()
-	if not debuff[classtable.FlameShock].up or debuff[classtable.FlameShock].refreshable and cooldown[classtable.FlameShock].ready then
-		return classtable.FlameShock
+	local inCombat = UnitAffectingCombat("player");
+	MaxDps:GlowCooldown(RT.EarthlivingWeapon, not earthLivingEnchantRemaining or (not inCombat and earthLivingEnchantRemaining <= 300));
+
+	if not debuff[RT.FlameShock].up or debuff[RT.FlameShock].refreshable then
+		return RT.FlameShock;
 	end
-	if talents[classtable.AcidRain] and cooldown[classtable.HealingRain].ready then
-		return classtable.HealingRain
+
+	local targets = MaxDps:SmartAoe();
+	if targets >= 3 then
+		return RT.ChainLightning;
 	end
-	if talents[classtable.LavaSurge] and buff[classtable.LavaSurgeBuff].up and cooldown[classtable.LavaBurst].ready then
-		return classtable.LavaBurst
+
+	if cooldown[RT.LavaBurst].ready then
+		return RT.LavaBurst;
 	end
-	if talents[classtable.Stormkeeper] and cooldown[classtable.Stormkeeper].ready then
-		return classtable.Stormkeeper
+
+	if targets >= 2 then
+		return RT.ChainLightning;
 	end
-	if cooldown[classtable.ChainLightning].ready then
-		return classtable.ChainLightning
-	end
-	if cooldown[classtable.LavaBurst].ready then
-		return classtable.LavaBurst
-	end
-	if cooldown[classtable.LightningBolt].ready then
-		return classtable.LightningBolt
-	end
+
+	return RT.LightningBolt;
 end
